bf586a9756cbff2b821af5feca3f7f70f69c3b90
merail <rail.me@google.com>
2021-12-05 00:31:24 +0300

    Checking and requesting permissions: READ_PHONE_STATE, READ_CALL_LOG, SYSTEM_ALERT_WINDOW

--- app/src/main/java/me/rail/incomingcallhandler/MainActivity.java
+++ app/src/main/java/me/rail/incomingcallhandler/MainActivity.java
@@ -1,14 +1,62 @@
 package me.rail.incomingcallhandler;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final ActivityResultLauncher<Intent> alertWindowPermissionResultLauncher =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (!Settings.canDrawOverlays(this)) {
+                            checkSystemAlertWindowPermission();
+                        }
+                    }
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkSystemAlertWindowPermission();
+        checkPermissions();
+    }
+
+    private void checkSystemAlertWindowPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                alertWindowPermissionResultLauncher.launch(intent);
+            }
+        }
+    }
+
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_CALL_LOG)  != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.READ_PHONE_STATE,
+                    Manifest.permission.READ_CALL_LOG},
+                    0);
+        }
     }
 }

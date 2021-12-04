966ac6fed04f257f44f8eeecd796cefb39595f92
merail <rail.me@google.com>
2021-12-04 22:46:00 +0300

    Initial commit

---
+++ app/src/main/java/me/rail/incomingcallhandler/MainActivity.java
@@ -0,0 +1,14 @@
+package me.rail.incomingcallhandler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

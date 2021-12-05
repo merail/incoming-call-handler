f2c8ee4c93042b61020a789706078fbd2cfd4bd5
merail <rail.me@google.com>
2021-12-06 00:54:53 +0300

    Add dragging availability for info window

--- app/src/main/java/me/rail/incomingcallhandler/IncomingCallBroadcastReceiver.java
+++ app/src/main/java/me/rail/incomingcallhandler/IncomingCallBroadcastReceiver.java
@@ -8,6 +8,7 @@ import android.os.Build;
 import android.telephony.TelephonyManager;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
@@ -17,6 +18,10 @@ import android.widget.TextView;
 public class IncomingCallBroadcastReceiver extends BroadcastReceiver {
     private static WindowManager windowManager;
     private static ViewGroup windowLayout;
+
+    private WindowManager.LayoutParams params;
+    private float x;
+    private float y;
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -39,13 +44,38 @@ public class IncomingCallBroadcastReceiver extends BroadcastReceiver {
     private void showWindow(final Context context, String phone) {
         windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         windowLayout = (ViewGroup) View.inflate(context, R.layout.window_call_info, null);
+        getLayoutParams();
+        setOnTouchListener();
 
         TextView numberTextView = windowLayout.findViewById(R.id.number);
         numberTextView.setText(phone);
         Button cancelButton = windowLayout.findViewById(R.id.cancel);
         cancelButton.setOnClickListener(view -> closeWindow());
 
-        windowManager.addView(windowLayout, getLayoutParams());
+        windowManager.addView(windowLayout, params);
+    }
+
+    private void setOnTouchListener() {
+        windowLayout.setOnTouchListener((view, event) -> {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    x = event.getRawX();
+                    y = event.getRawY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    params.x = params.x - (int) (x - event.getRawX());
+                    params.y = params.y - (int) (y - event.getRawY());
+                    windowManager.updateViewLayout(windowLayout, params);
+                    x = event.getRawX();
+                    y = event.getRawY();
+                    break;
+                case MotionEvent.ACTION_UP:
+                    view.performClick();
+                default:
+                    break;
+            }
+            return false;
+        });
     }
 
     private void closeWindow() {
@@ -62,8 +92,8 @@ public class IncomingCallBroadcastReceiver extends BroadcastReceiver {
         return WindowManager.LayoutParams.TYPE_PHONE;
     }
 
-    private WindowManager.LayoutParams getLayoutParams() {
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+    private void getLayoutParams() {
+        params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 getWindowsTypeParameter(),
@@ -77,8 +107,6 @@ public class IncomingCallBroadcastReceiver extends BroadcastReceiver {
         params.gravity = Gravity.CENTER;
         params.format = 1;
         params.width = getWindowWidth();
-
-        return params;
     }
 
     private int getWindowWidth() {

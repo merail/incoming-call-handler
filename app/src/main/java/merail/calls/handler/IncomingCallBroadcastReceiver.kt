46e27688b522608d44cc5e0a13760c2e9bfbf13d
merail <r.mesherov@blanc.ru>
2024-05-03 10:28:18 +0300

    Sending intent from receiver to service for closing window

--- app/src/main/java/merail/calls/handler/IncomingCallBroadcastReceiver.kt
+++ app/src/main/java/merail/calls/handler/IncomingCallBroadcastReceiver.kt
@@ -22,15 +22,23 @@ class IncomingCallBroadcastReceiver : BroadcastReceiver() {
         get() = getStringExtra(TelephonyManager.EXTRA_STATE) == TelephonyManager.EXTRA_STATE_IDLE ||
                 getStringExtra(TelephonyManager.EXTRA_STATE) == TelephonyManager.EXTRA_STATE_OFFHOOK
 
+    private val Intent.needToCloseWindowForApiN: Boolean
+        get() = Build.VERSION.SDK_INT > Build.VERSION_CODES.M && needToCloseWindow
+
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == TelephonyManager.ACTION_PHONE_STATE_CHANGED) {
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
-                val phoneNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)
-                when {
-                    phoneNumber == null -> Unit
-                    intent.needToShowWindow -> incomingCallAlert.showWindow(context, phoneNumber)
-                    intent.needToCloseWindow -> incomingCallAlert.closeWindow()
-                }
+            val phoneNumber = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+                intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)
+            } else {
+                null
+            }
+            when {
+                intent.needToCloseWindowForApiN -> context
+                    .applicationContext
+                    .startService(Intent(context, IncomingCallService::class.java))
+                phoneNumber == null -> Unit
+                intent.needToShowWindow -> incomingCallAlert.showWindow(context, phoneNumber)
+                intent.needToCloseWindow -> incomingCallAlert.closeWindow()
             }
         }
     }

d77d195e754635bfbde8068cfe9753322fd1d4f0
Adam Drozdz <adam.drozdz@iterative.pl>
2024-07-26 13:01:27 +0200

    wip

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,5 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("com.github.merail:request-permissions-tool:1.1.0")
+    implementation("com.google.firebase:firebase-crashlytics-buildtools:3.0.2")
 }

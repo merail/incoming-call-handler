b143f583052d7d4fd2ca5accdcb54058cdcd6cb8
merail <r.mesherov@blanc.ru>
2024-11-14 12:08:43 +0300

    Update Compose BOM dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("androidx.activity:activity-compose:1.9.3")
-    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.11.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.compose.ui:ui-tooling-preview")
